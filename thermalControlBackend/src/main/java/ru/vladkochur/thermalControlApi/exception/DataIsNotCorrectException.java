8f45515d6cd9231f8830be38f41b5408faf3d554
Vladislav K <148258123+VladislavKoch@users.noreply.github.com>
2024-06-13 19:03:18 +0300

    Delete thermalControlBackend/src/main directory

--- thermalControlBackend/src/main/java/ru/vladkochur/thermalControlApi/exception/DataIsNotCorrectException.java
+++
@@ -1,7 +0,0 @@
-package ru.vladkochur.thermalControlApi.exception;
-
-public class DataIsNotCorrectException extends RuntimeException{
-    public DataIsNotCorrectException(String message) {
-        super(message);
-    }
-}
--- thermalControlBackend/src/main/java/ru/vladkochur/thermalControlApi/service/serviceInterface/SensorSetupService.java
+++
@@ -1,7 +0,0 @@
-package ru.vladkochur.thermalControlApi.service.serviceInterface;
-
-import ru.vladkochur.thermalControlApi.entity.SensorSetup;
-
-public interface SensorSetupService {
-    public SensorSetup getSetupBySerial(Integer serial) ;
-    }
